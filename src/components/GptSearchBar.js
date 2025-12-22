0ae6ca8d4cb26ba58b9afe6c94a805083d05a65a
Aditya Batchu <batchuaditya369@gmail.com>
2025-12-22 15:38:52 +0530

    get results from api and store it to the Store

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -2,11 +2,24 @@ import { useSelector } from "react-redux";
 import lang from "../utils/languageConstants";
 import { useRef } from "react";
 import { geminiModel } from "../utils/gemini";
+import { API_OPTIONS } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { addGptMovieResult } from "../utils/gptSlice";
 
 const GptSearchBar = () => {
   const language = useSelector((store) => store.config.lang);
   const searchText = useRef(null);
+  const dispatch = useDispatch();
 
+  const searchMovieTMDB = async (movie) => {
+    const data = await fetch(
+      'https://api.themoviedb.org/3/search/movie?query='+movie+'&include_adult=false&language=en-US&page=1',
+      API_OPTIONS
+    );
+    const json = await data.json();
+
+    return json.results;
+  };
   const handleGptSearchClick = async () => {
     const query = searchText.current.value;
 
@@ -19,21 +32,29 @@ const GptSearchBar = () => {
 
     try {
       const result = await geminiModel.generateContent(prompt);
-      
+
       // FIXED: Added await and () because text() is an async function
-      const responseText = await result.response.text(); 
+      const responseText = await result.response.text();
 
       // Convert "Movie 1, Movie 2" -> ["Movie 1", "Movie 2"]
-      const movieArray = responseText.split(",").map(m => m.trim());
+      const movieArray = responseText.split(",").map((m) => m.trim());
 
-      console.log("Gemini Movie Array:", movieArray);
-      
+      // console.log("Gemini Movie Array:", movieArray);
+
       // Next Step: dispatch(addGptMovieResult({movieNames: movieArray}));
-      
+
+      const promiseArray = movieArray.map((movie)=> searchMovieTMDB(movie));
+
+      const tmdbResults = await Promise.all(promiseArray);
+
+      console.log(tmdbResults);
+
+      dispatch(addGptMovieResult({movieNames:movieArray,tmdbResults:tmdbResults}));
+
+
     } catch (err) {
       console.error("Gemini Error:", err);
     }
-
 
   };
 
--- src/utils/gptSlice.js
+++ src/utils/gptSlice.js
@@ -4,14 +4,23 @@ const gptSlice = createSlice({
   name: "gpt",
   initialState: {
     showGptSearch: false,
+    movieNames:null,
+    tmdbResults:null,
+
   },
   reducers: {
     toggleGptSearchView: (state, action) => {
       state.showGptSearch = !state.showGptSearch;
     },
+    addGptMovieResult:(state,action)=>{
+      const {movieNames, tmdbResults} = action.payload;
+      state.movieNames = movieNames;
+      state.tmdbResults = tmdbResults;
+    },
+
   },
 });
 
-export const { toggleGptSearchView } = gptSlice.actions;
+export const { toggleGptSearchView, addGptMovieResult } = gptSlice.actions;
 
 export default gptSlice.reducer;
